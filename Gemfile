f47316ed2d57c329bc398b34fc5fc8fe98d26448
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2021-03-02 18:57:34 +0900

    set bootstrap with webpack

--- Gemfile
+++ Gemfile
@@ -65,11 +65,10 @@ gem 'line-bot-api'
 
 gem 'dotenv-rails'
 
-gem 'bootstrap', '~> 4.5'
-gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13'
 
 gem 'slim-rails'
 gem 'html2slim'
 
 gem 'pry-byebug'
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../stylesheets/application';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+
+$(function () {
+  $('.toast').toast({animation:true, autohide: true, delay:3000});
+});
