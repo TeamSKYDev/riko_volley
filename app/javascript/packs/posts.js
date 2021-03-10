c3bdf7563d1e0923b65a0bb713d2b7c25cde73b4
sato9818 <kouki9818@gmail.com>
2021-03-10 22:44:09 +0900

    finish noti

--- app/javascript/packs/posts.js
+++ app/javascript/packs/posts.js
@@ -6,8 +6,12 @@ $win.on('load resize', function() {
   if (windowWidth < 768) {
     $('.d-flex.changeable').removeClass('justify-content-between');
     $('.d-flex.changeable').addClass('flex-column');
+    $('#notification .col-8.changeable').removeClass('col-8');
+    $('.btn').addClass('btn-sm');
   }else{
     $('.d-flex.changeable').removeClass('flex-column');
     $('.d-flex.changeable').addClass('justify-content-between');
+    $('#notification .col-8.changeable').addClass('col-8');
+    $('.btn').removeClass('btn-sm');
   }
 });
