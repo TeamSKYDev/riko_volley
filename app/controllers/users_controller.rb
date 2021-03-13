75db43833873b8cb0cf6468a8414fe42f1eb2f10
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2021-03-13 11:42:24 +0900

    fix some errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :check_admin
+  before_action :set_user, only: [:index, :destroy]
+  before_action :set_users, only: [:index]
 
-  def index
-    @user = User.new
-    @users = User.all
-  end
+  def index; end
 
   def create
     @user = User.new(user_params)
-    @user.save!
-    redirect_to users_path
+    if @user.save
+      flash[:notice] = "登録完了"
+      redirect_to users_path
+    else
+      set_users
+      render "index"
+    end
   end
 
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+    if @user.destroy
+      flash[:notice] = "削除しました"
+    end
     redirect_to users_path
   end
 
@@ -28,5 +33,12 @@ class UsersController < ApplicationController
     unless current_user.is_admin == true
       redirect_to posts_path
     end
+  end
+
+  def set_user
+    @user = User.find_or_initialize_by(id: params[:id])
+  end
+  def set_users
+    @users = User.all
   end
 end
